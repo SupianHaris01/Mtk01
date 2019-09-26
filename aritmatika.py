2a5e8449c6c7f43b14b974d3966edd96c57b3db4
SupianHaris01 <55823708+SupianHaris01@users.noreply.github.com>
2019-09-26 20:36:57 +0800

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,26 @@
+#Aritmatika#
+print('>>\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\<<')
+print('||    Author BY |> CNC ~ 1S-H2A7 ||')
+print('||    Thnk To   |> All Member    ||')
+print('||==<<<<<<<<<<<<||||>>>>>>>>>>>==||')
+print('||            -=TEAM=-           ||')
+print('||           >> .CNC <<          ||')
+print('||      CyberNetworkConnection   ||')
+print('||=================================')
+print('|| >>   Operator_ARITMATIKA   << ||')
+print('>>\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\<<')
+#GAS
+a = input('Masukkan Angka A ->> ')
+b = input('Masukkan Angka B ->> ')
+c = a + b
+print "Tambahan    |> %d + %d = %d" % (a,b,c)
+c = a - b
+print "Pengurangan |> %d - %d = %d" % (a,b,c)
+c = a * b 
+print "Perkalian   |> %d x %d = %d" % (a,b,c)
+c = a / b
+print "Pembagian   |> %d : %d = %d" % (a,b,c)
+c = a % b
+print "Sisa Bagi   |> %d %% %d = %d" % (a,b,c)
+c = a ** b
+print "Berpangkat  |> %d ** %d = %d" % (a,b,c)
---
+++ bitwise.py
@@ -0,0 +1,22 @@
+print('>>\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\<<')
+print('||    Author BY |> CNC ~ 1S-H2A7 ||')
+print('||    Thnk To   |> All Member    ||')
+print('||==<<<<<<<<<<<<||||>>>>>>>>>>>==||')
+print('||            -=TEAM=-           ||')
+print('||           >> .CNC <<          ||')
+print('||      CyberNetworkConnection   ||')
+print('||=================================')
+print('|| >>   Operator_BITWISE      << ||')
+print('>>\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\<<')
+a = input('Masukkan Angka A ->> ')
+b = input('Masukkan Angka B ->> ')
+c = a & b
+print "Operasi AND                              a & b = %s" % c
+c = a | b
+print "Operasi OR                               a | b = %s" % c
+c = a ^ b
+print "Operasi XOR                              a ^ b = %s" % c
+c = ~a
+print "Operasi Not                                 ~a = %s" % c
+c = a << b
+print "Operasi shift left (tukar posisi biner) a << b = %s" % c
